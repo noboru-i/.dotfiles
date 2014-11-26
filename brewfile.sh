10659a730e5958f20cb66aea15a83039bff76f07
noboru-i <ishikura.noboru@gmail.com>
2014-11-26 22:33:43 +0900

    不要な記述を削除

--- brewfile.sh
+++ brewfile.sh
@@ -1,71 +1,71 @@
-brew tap homebrew/versions|| true
-brew tap homebrew/binary || true
-brew tap peco/peco || true
-brew tap sanemat/font || true
+brew tap homebrew/versions
+brew tap homebrew/binary
+brew tap sanemat/font
 brew update
 brew upgrade
 brew upgrade brew-cask
 
-brew install automake || true
-brew install gcc || true
-brew install caskroom/cask/brew-cask || true
-brew install git || true
-brew install tig || true
-brew install tree || true
-brew install zsh || true
-brew install vim || true
-brew install gradle || true
-brew install wireshark --with-x || true
-brew install pyenv-virtualenv || true
-brew install go || true
-brew install plenv || true
-brew install perl-build || true
-brew install jq || true
-brew install terminal-notifier || true
-brew install colordiff || true
-brew install fontforge --use-gcc --without-python || true
-brew install ricty || true
-brew install --HEAD hub || true
+brew install automake
+brew install gcc
+brew install caskroom/cask/brew-cask
+brew install git
+brew install tig
+brew install tree
+brew install zsh
+brew install vim
+brew install gradle
+brew install wireshark --with-x
+brew install pyenv-virtualenv
+brew install go
+brew install plenv
+brew install perl-build
+brew install jq
+brew install terminal-notifier
+brew install colordiff
+brew install fontforge --use-gcc --without-python
+brew install ricty
+brew install --HEAD hub
 
-brew install peco || true
+brew tap peco/peco
+brew install peco
 
-brew install rbenv || true
-brew install ruby-build || true
+brew install rbenv
+brew install ruby-build
 
-brew install homebrew/dupes/apple-gcc42 || true
-brew install homebrew/php/phplint || true
+brew install homebrew/dupes/apple-gcc42
+brew install homebrew/php/phplint
 
-brew cask install skype || true
-brew cask install skitch || true
-brew cask install evernote || true
+brew cask install skype
+brew cask install skitch
+brew cask install evernote
 
-brew cask install alfred || true
-brew cask install dash || true
+brew cask install alfred
+brew cask install dash
 
-brew cask install cyberduck || true
-brew cask install imageoptim || true
-brew cask install speedlimit || true
-brew cask install google-japanese-ime || true
+brew cask install cyberduck
+brew cask install imageoptim
+brew cask install speedlimit
+brew cask install google-japanese-ime
 
-brew cask install google-chrome-beta || true
-brew cask install google-chrome-canary || true
-brew cask install firefox || true
+brew cask install google-chrome-beta
+brew cask install google-chrome-canary
+brew cask install firefox
 
-brew cask install virtualbox || true
-brew cask install vagrant || true
+brew cask install virtualbox
+brew cask install vagrant
 
-brew cask install atom || true
-brew cask install iterm2 || true
+brew cask install atom
+brew cask install iterm2
 
-brew cask install android-studio || true
-brew cask install genymotion || true
+brew cask install android-studio
+brew cask install genymotion
 
 brew tap caskroom/homebrew-versions
-brew cask install sublime-text3 || true
+brew cask install sublime-text3
 
-brew cask install github || true
+brew cask install github
 
-brew cask install macdown || true
+brew cask install macdown
 
 brew cleanup
 brew cask cleanup
