4af54212ed71a5171c2b81bbb37921c322a9fb9c
noboru-i <ishikura.noboru@gmail.com>
2021-02-22 23:53:26 +0900

    install asdf plugins

--- install.sh
+++ install.sh
@@ -15,7 +15,16 @@ do
   ln -s ~/.dotfiles/$i ~/
 done
 
+## applications
 brew bundle
+
+## asdf
+for plugin in $(cat .tool-versions | sed s/' .*$'//); do
+  asdf plugin-list | grep $plugin > /dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    asdf plugin-add $plugin
+  fi
+done
 asdf install
 
 ## Flutter
