e08d9799f3d555c68542ce87c80dabe436860436
noboru-i <ishikura.noboru@gmail.com>
2026-01-19 22:54:12 +0900

    update aliases: add time-style option to ls and ll commands

--- dot_zsh/30_aliases.zsh
+++ dot_zsh/30_aliases.zsh
@@ -1,4 +1,4 @@
-alias ls='eza --icons -a --group-directories-first'
-alias ll='eza --icons -al --group-directories-first'
+alias ls='eza --icons -a --group-directories-first --time-style=long-iso'
+alias ll='eza --icons -al --group-directories-first --time-style=long-iso'
 
 alias tree='eza --icons -a -T -L 2'
