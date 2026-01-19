eb59cf1ff6a89649a65e104e14e3d26231677d5b
noboru-i <ishikura.noboru@gmail.com>
2026-01-19 23:19:05 +0900

    update aliases: add hyperlink option to ls and ll commands

--- dot_zsh/30_aliases.zsh
+++ dot_zsh/30_aliases.zsh
@@ -1,4 +1,4 @@
-alias ls='eza --icons -a --group-directories-first --time-style=long-iso'
-alias ll='eza --icons -al --group-directories-first --time-style=long-iso'
+alias ls='eza --icons -a --group-directories-first --time-style=long-iso --hyperlink'
+alias ll='eza --icons -al --group-directories-first --time-style=long-iso --hyperlink'
 
 alias tree='eza --icons -a -T -L 2'
