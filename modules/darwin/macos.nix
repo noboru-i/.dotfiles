995abc8ce6c7a7682c91de743f53bcb6d7781cda
noboru-i <ishikura.noboru@gmail.com>
2026-03-24 22:51:09 +0900

    fix: update keyboard and trackpad preferences in macOS settings

--- modules/darwin/macos.nix
+++ modules/darwin/macos.nix
@@ -19,6 +19,16 @@
       InitialKeyRepeat = 15;
       "com.apple.sound.beep.feedback" = 0;
       "com.apple.keyboard.fnState" = true;
+      # キーを長押ししたときにアクセント記号メニューではなくキーリピートを使用
+      ApplePressAndHoldEnabled = false;
+      # キーボード: ユーザ辞書設定
+      NSAutomaticSpellingCorrectionEnabled = false;
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      # トラックパッド: スクロールの方向:ナチュラル -> OFF
+      "com.apple.swipescrolldirection" = false;
+      # トラックパッド: 軌跡の速さ -> 一番速く
+      "com.apple.trackpad.scaling" = 3.0;
     };
     trackpad = {
       Clicking = true;
@@ -26,6 +36,15 @@
     CustomUserPreferences = {
       "com.apple.dock" = {
         "expose-animation-duration" = 0.1;
+      };
+      # 一般: Handoff -> OFF
+      "com.apple.coreservices.useractivityd" = {
+        ActivityAdvertisingAllowed = false;
+        ActivityReceivingAllowed = false;
+      };
+      # 共有: AirPlayレシーバー -> OFF
+      "com.apple.controlcenter" = {
+        AirplayRecieverEnabled = false;
       };
     };
   };
